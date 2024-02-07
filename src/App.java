4e874e7d3b1f1ae74cbc6dcb34c99f7b4c1510a3
alothmanmj <alothman.maj@gmail.com>
2024-02-07 15:37:06 +0300

    Add good day print

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Good Day Everyone");
         generateGroups(readStudents(),4);
         
     }
