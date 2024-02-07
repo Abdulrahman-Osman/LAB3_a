3572a74712706a4b4cab6afeab02cc9743274665
alothmanmj <alothman.maj@gmail.com>
2024-02-07 14:50:33 +0300

    Add Student List file and a method to read it into an array

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println(readStudents());
+    }
+
+    public static ArrayList<String> readStudents(){
+        ArrayList studetns = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(new File("Student List.txt"));
+            
+            while(sc.hasNextLine()){
+                studetns.add(sc.nextLine());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+
+
+        return studetns;
     }
 }
