92914da8101e989e904af50c7343417a8fa0d73e
Abdulmjeed Abdullah Alothman <abdulmjeed.alothman@kfupm.edu.sa>
2024-02-07 15:31:39 +0300

    Change the print

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello,from sec53");
         generateGroups(readStudents(),4);
         
     }
