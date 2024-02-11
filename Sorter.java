c496b2b8c74116dd13664e2e2a988ec88e0f09f4
unknown <gzrazak@gmail.com>
2024-02-11 21:06:56 +0300

    Implement selectionSort

--- Sorter.java
+++ Sorter.java
@@ -1,1 +1,18 @@
- 
+public class Sorter {
+    public void selectionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+}
