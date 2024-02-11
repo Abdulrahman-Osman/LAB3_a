10fe9637a1da59c223f85bac9aa3f2563eb7e535
Abdulrahman Alwazir <aboda7iim.17@gmail.com>
2024-02-11 21:57:28 +0300

    Implemented insertionSort method

--- Sorter.java
+++ Sorter.java
@@ -15,6 +15,21 @@ public class Sorter {
             array[i] = temp;
         }
     }
+    public void insertionSort(int[] array) {
+        int n = array.length;
+    
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+    
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+    
+            array[j + 1] = key;
+        }
+    }
     public void sort(int[] array) {
         selectionSort(array);
     }
